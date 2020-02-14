a19b75396d77488d368c3a4c9cb3c11f2aff081a
Viktor Podzigun <viktor.podzigun@gmail.com>
2020-02-14 09:46:52 +0100

    Added headerTitle to ScreenOptions

--- navigation/src/main/scala/scommons/react/navigation/stack/raw/ScreenOptions.scala
+++ navigation/src/main/scala/scommons/react/navigation/stack/raw/ScreenOptions.scala
@@ -1,11 +1,14 @@
 package scommons.react.navigation.stack.raw
 
+import scommons.react._
+
 import scala.scalajs.js
 
 trait ScreenOptions extends js.Object {
 
   val title: js.UndefOr[String] = js.undefined
   val headerTintColor: js.UndefOr[String] = js.undefined
+  val headerTitle: js.UndefOr[js.Function1[js.Dynamic, ReactElement]] = js.undefined
   val headerStyle: js.UndefOr[js.Object] = js.undefined
   val headerShown: js.UndefOr[Boolean] = js.undefined
   val gestureEnabled: js.UndefOr[Boolean] = js.undefined
